52630304a47cc9cf0991551f7b3b2ff98721ad0d
Mikita Buinavets <mikita.buinavets@arua.co>
2024-12-15 22:17:46 +0300

    Add custom exception StudentNotFoundException

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -6,15 +6,24 @@ public class StudentManager {
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
   public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+    Student stud = Student.getValueOf(studentID);
+    if (stud != null) {
+      return stud;
+    } else {
+      throw new StudentNotFoundException( "Could not find student with ID " + studentID);
+    }
   }
 
   public static void main(String[] args) {
     StudentManager manager = new StudentManager();
 
-    for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
+    try {
+      for (int i = 0; i < IDs.length; i++) {
+        Student student = manager.find(IDs[i]);
+        System.out.println("Student name " + student.getName());
+      }
+    } catch (StudentNotFoundException ex) {
+      System.err.print(ex);
     }
 
   }
